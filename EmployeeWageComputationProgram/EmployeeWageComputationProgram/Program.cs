b4897f3621d46bf64538398a4de67dedc328d806
qismat <qismatnisha6243@gmail.com>
2022-12-26 22:50:25 +0530

    [qismat] Add : UC14-TotalWage

--- EmployeeWageComputationProgram/EmployeeWageComputationProgram/Program.cs
+++ EmployeeWageComputationProgram/EmployeeWageComputationProgram/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EmployeeWageComputationProgram
+﻿using System;
+
+namespace EmployeeWageComputationProgram
 {
     public class Program
     {
@@ -7,10 +9,12 @@
             Console.WriteLine("Welcome to Employeewage program.");
 
             EmpWageBuilder empWageBuilder = new EmpWageBuilder();
-
             empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
             empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
             empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for Dmart company :{0} ", empWageBuilder.getTotalWage("Dmart"));
+            Console.WriteLine("Total Wage for Reliance company :{0} ", empWageBuilder.getTotalWage("Reliance"));
+
         }
     }
 }
